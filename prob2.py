67606862b9694b8a9d14965a9b6747f8f2d6aad5
aldafari <aldafari@memphis.edu>
2024-02-13 21:52:14 -0600

    Created sorted list of strings via lambda

--- prob2.py
+++ prob2.py
@@ -1,2 +1,5 @@
-strings = ["Hello", "Hi", "Greatings", "salutations"]
-print(strings)
+
+strings_list = ['Hello', 'Hi', 'Greatings', 'salutations']
+sorted_Strings = sorted(strings_list, key=lambda x: len(x))
+print(sorted_Strings)
+
